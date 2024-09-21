6006a978918da41a7e0b1bc70af23b66fb8cb237
yousefatef22040 <163738917+yousefatef22040@users.noreply.github.com>
2024-09-21 18:29:52 +0300

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,7 @@
+let el = document.querySelector(".scroller")
+let height = document.documentElement.scrollHeight - document.documentElement.clientHeight 
+
+window.addEventListener("scroll", () => {
+    let scrollTop = document.documentElement.scrollTop;
+    el.style.width = `${(scrollTop / height) * 100}%`
+})
